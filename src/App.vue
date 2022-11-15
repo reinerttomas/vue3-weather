8719b5bf41e54d11655003366cf6afdc51a24adc
Tomáš Reinert <reinerttomas@gmail.com>
2022-11-15 12:30:45 +0100

    navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <TheNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import TheNavigation from '@/components/TheNavigation.vue';
 </script>
