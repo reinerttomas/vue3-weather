632f4d960f8f82fd6e79023b595a19f00d43937f
Tomáš Reinert <reinerttomas@gmail.com>
2022-12-10 11:16:11 +0100

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <TheNavigation />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component"></component>
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -9,3 +13,14 @@
 import { RouterView } from 'vue-router';
 import TheNavigation from '@/components/TheNavigation.vue';
 </script>
+
+<style>
+.page-enter-active,
+page-leave-active {
+  transition: 600ms ease all;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
